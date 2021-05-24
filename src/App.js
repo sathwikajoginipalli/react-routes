447d909091ef5ddb7b216682fa8c559da4d0d799
sathwikajoginipalli <71289487+sathwikajoginipalli@users.noreply.github.com>
2021-05-24 14:38:24 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, NavLink} from 'react-router-dom'
+import Route from 'react-router-dom/Route'
+import User from './components/User'
+class App extends Component {
+  render() { 
+    return ( <Router>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/user">User</NavLink></li>
+      </ul>
+      
+      <Route path="/" exact strict render={
+        ()=>{
+          return(<h1>I'm Home</h1>)
+        }
+      }></Route>
+        <Route path="/about" exact strict render={
+        ()=>{
+          return(<h1>I'm About</h1>)
+        }
+      }></Route>
+      <Route path="/user" exact strict render={
+        ()=>{
+          return(<User/>)
+        }
+      }></Route>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    </Router> );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/User.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class User extends Component {
+    render() { 
+        return ( <h1>Hello meeee</h1> );
+    }
+}
+ 
+export default User;
